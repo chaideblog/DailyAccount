13fe1f901ddd4bf7a50dae936690ee334065648f
chaideblog <chai_jinlong@163.com>
2017-06-18 23:41:13 +0800

    修改了“添加收入”的默认时间

--- UI/Dialog_AddIncome.py
+++ UI/Dialog_AddIncome.py
@@ -7,6 +7,8 @@
 # WARNING! All changes made in this file will be lost!
 
 from QtCreator import Ui_Dialog_AddIncome
+from Tools import TimeTools
+from PyQt5 import QtCore
 
 class Dialog_AddIncome(Ui_Dialog_AddIncome.Ui_Dialog_AddIncome):
     def __init__(self):
@@ -18,6 +20,7 @@ class Dialog_AddIncome(Ui_Dialog_AddIncome.Ui_Dialog_AddIncome):
         print("Hello World")
 
     def addUi(self):
+        self.dateEdit_2.setDateTime(QtCore.QDateTime(QtCore.QDate(TimeTools.getCurrentYear(), TimeTools.getCurrentMonth(), TimeTools.getCurrentDay()), QtCore.QTime(0, 0, 0)))
         self.buttonBox.accepted.connect(self.test)  # 链接"添加收入"中Ok信号和槽
         self.buttonBox.rejected.connect(self.close)  # 链接"添加收入"中Cancel信号和槽
 
